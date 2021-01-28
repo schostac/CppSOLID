e5869187a944a2c751ef50222cc17a2a7499ba33
shostatskyi <shostatscky.andriy@yandex.ua>
2021-01-28 23:20:37 +0100

    Correction to sequence diagram, etc.

--- project/install_dependencies.sh
+++ project/install_dependencies.sh
@@ -5,17 +5,21 @@ PROJECT_DIR=$(pwd)
 sudo add-apt-repository ppa:mhier/libboost-latest
 sudo apt update
 
-sudo apt install libboost1.68-dev
+sudo apt install libboost1.70-dev
 cat /usr/include/boost/version.hpp | grep "BOOST_LIB_VERSION"
 
 sudo apt-get install -y libgtest-dev google-mock cmake valgrind
 
 cd /usr/src/gtest/
 sudo cmake CMakeLists.txt
-sudo make install -j4
+sudo make -j4
+sudo cp lib/*.a /usr/lib
+sudo cp -r include/gtest /usr/include
 
 cd ../gmock
 sudo cmake CMakeLists.txt
-sudo make install -j4
+sudo make -j4
+sudo cp lib/* /usr/lib
+sudo cp -r include/gmock /usr/include
 
 cd $PROJECT_DIR
